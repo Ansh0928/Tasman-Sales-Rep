d0401c3b1e8ffce58ff6b99b48524a0db6c11687
Anshumaan Saraf <anshumaansaraf24@gmail.com>
2026-03-04 22:16:46 +1000

    Initial Commit

---
+++ Tasman-Sales-Rep/Tasman_Sales_RepApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tasman_Sales_RepApp.swift
+//  Tasman-Sales-Rep
+//
+//  Created by Tasman Star Seafood  on 4/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct Tasman_Sales_RepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
